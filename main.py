bb73182621d1c2407db7556cc4e4f93bf5e1abe0
RockStoneRock-droid <chasegameac@gmail.com>
2024-04-17 16:43:17 -0400

    parserupdate

--- main.py
+++ main.py
@@ -15,19 +15,49 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def evaluate(self):
+        if self.token == "NUMBER":
+            return int(self.value)
+        elif self.token == "PLUS":
+            return self.left.evaluate() + self.right.evaluate()
+        elif self.token == "MINUS":
+            return self.left.evaluate() - self.right.evaluate()
+        elif self.token == "MULTIPLY":
+            return self.left.evaluate() * self.right.evaluate()
+        elif self.token == "DIVIDE":
+            return self.left.evaluate() / self.right.evaluate()
+
 def parserEX(srcList):
-    print(srcList)
+    #print(srcList)
     if len(srcList) > 2:
         leftTree = TreeNode(srcList[0])
         op = TreeNode(srcList[1])
         rightTree = parserEX(srcList[2:])
+        op.left = leftTree
+        op.right = rightTree
+        rootNode = op
+    elif len(srcList) == 2:
+        leftTree = TreeNode(srcList[0])
+        op = TreeNode(srcList[1])
+        rootNode = op
     else:
-        print(srcList[0])
         return TreeNode(srcList[0])
-    op.left = leftTree
-    op.right = rightTree
-    rootNode = op
 
     return rootNode
 
+    # print(srcList)
+    # if len(srcList) > 2:
+    #     leftTree = TreeNode(srcList[0])
+    #     op = TreeNode(srcList[1])
+    #     rightTree = parserEX(srcList[2:])
+    # else:
+    #     print(srcList[0])
+    #     return TreeNode(srcList[0])
+    # op.left = leftTree
+    # op.right = rightTree
+    # rootNode = op
+
+    # return rootNode
+
 main()
+
