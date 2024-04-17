13afb4d28b2e75d4a6d041d1b2efc752a46ff326
Chase <76849972+chase-c-dev@users.noreply.github.com>
2024-04-17 13:11:37 -0700

    added in parser

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from tokenizing import tokenize
 
 def main():
-    srcCode = "123 + 55 + 65"
+    srcCode = "1 + 5 + 1 + 5 + 7"
     final_list = tokenize(srcCode)
     for item in final_list:
         print(item)
+
+    parserEX(final_list)
+
+class TreeNode:
+    def __init__(self,srcToken):
+        self.value = srcToken[0]
+        self.token = srcToken[1]
+        self.left = None
+        self.right = None
+
+def parserEX(srcList):
+    print(srcList)
+    if len(srcList) > 2:
+        leftTree = TreeNode(srcList[0])
+        op = TreeNode(srcList[1])
+        rightTree = parserEX(srcList[2:])
+    else:
+        print(srcList[0])
+        return TreeNode(srcList[0])
+    op.left = leftTree
+    op.right = rightTree
+    rootNode = op
+
+    return rootNode
 
 main()
