6ca9e2b04e15331a9f913a9ecd64277c8ed0f5d3
Chase <76849972+chase-c-dev@users.noreply.github.com>
2024-04-19 12:35:29 -0400

    Fixed Multidigit Issue In Parser

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from tokenizing import tokenize
 
 def main():
-    srcCode = "22 + 15 + 2"
+    srcCode = "10 + 5 * 5 - 5"
     final_list = tokenize(srcCode)
     for item in final_list:
         print(item)
     total = parserEX(final_list)
-    print(total)
+    print("the answer is: ", total)
 
 class TreeNode:
     def __init__(self,srcToken):
@@ -21,10 +21,10 @@ def parserEX(srcList):
         i = 0
         while i < len(srcList):
             if srcList[i][0] == '*':
-                leftTree = srcList[i-1] # Gets the value before the operator
-                rightTree = srcList[i+1] # Gets the value after the operator
-                result = str(int(leftTree[0][0]) * int(rightTree[0][0])) # multiplys the two values 
-                srcList[i] = [[result, 'NUMB']] 
+                leftTree = srcList[i-1][0] # Gets the value before the operator
+                rightTree = srcList[i+1][0] # Gets the value after the operator
+                result = str(int(leftTree) * int(rightTree)) # multiplys the two values 
+                srcList[i] = [result, 'NUMB'] 
                 del srcList[i+1]
                 del srcList[i-1]
                 return parserEX(srcList)
@@ -32,10 +32,10 @@ def parserEX(srcList):
         i = 0
         while i < len(srcList):
             if srcList[i][0] == '/':
-                leftTree = srcList[i-1] # Gets the value before the operator
-                rightTree = srcList[i+1] # Gets the value after the operator
-                result = str(int(int(rightTree[0][0]) / int(leftTree[0][0]))) # divides the two values, it int casts meaning that you will not see decimals
-                srcList[i] = [[result, 'NUMB']] 
+                leftTree = srcList[i-1][0] # Gets the value before the operator
+                rightTree = srcList[i+1][0] # Gets the value after the operator
+                result = str(int(int(rightTree) / int(leftTree))) # divides the two values, it int casts meaning that you will not see decimals
+                srcList[i] = [result, 'NUMB'] 
                 del srcList[i+1]
                 del srcList[i-1]
                 return parserEX(srcList)
@@ -45,14 +45,9 @@ def parserEX(srcList):
             if srcList[i][0] == '+':
                 leftTree = srcList[i-1][0] # Gets the value before the operator
                 rightTree = srcList[i+1][0] # Gets the value after the operator
-                # if leftTree[1] != 'NUMB':
-                #     tempTree = leftTree[0] + leftTree[1]
-                #     print(leftTree[0])
-                #     leftTree[0] = tempTree
-                #print(leftTree[1][0])
-                print(leftTree[0][0])
-                result = str(int(leftTree[0]) + int(rightTree)) # adds the two values
-                srcList[i] = [[result, 'NUMB']] 
+                print(srcList)
+                result = str(int(leftTree) + int(rightTree)) # adds the two values
+                srcList[i] = [result, 'NUMB'] 
                 del srcList[i+1]
                 del srcList[i-1]
                 return parserEX(srcList)
@@ -60,16 +55,20 @@ def parserEX(srcList):
         i = 0
         while i < len(srcList):
             if srcList[i][0] == '-':
-                leftTree = srcList[i-1] # Gets the value before the operator
-                rightTree = srcList[i+1] # Gets the value after the operator
-                result = str(int(leftTree[0][0]) - int(rightTree[0][0])) # subtracts the two values
-                srcList[i] = [[result, 'NUMB']] 
+                leftTree = srcList[i-1][0] # Gets the value before the operator
+                rightTree = srcList[i+1][0] # Gets the value after the operator
+                result = str(int(leftTree) - int(rightTree)) # subtracts the two values
+                srcList[i] = [result, 'NUMB'] 
                 del srcList[i+1]
                 del srcList[i-1]
                 return parserEX(srcList) 
             i += 1
     else:
         return srcList[0][0]
+        
+
+
+
 
 main()
 
